7e4c5e9420b6ba9fa89bafbd6324e123f149a0d8
jay <jay@ocard.co>
2021-07-24 22:17:48 +0800

    加入 jQuery UI 日期選擇

--- js/all.js
+++ js/all.js
@@ -1,8 +1,13 @@
 $(function() {
+  // 手機選單
   $('.menu-wrapper').click(function(e) {
     e.preventDefault();
 
     $('.hamburger-menu').toggleClass('open');
     $('.navbar-nav').slideToggle();
   });
+  
+  // 投件清單日期選擇
+  $('#startDate').datepicker();
+  $('#endDate').datepicker();
 });
