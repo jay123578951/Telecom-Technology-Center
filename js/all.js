9c2ed4efb0b86089d4374371e8fee8448e11bd64
jay <jay@ocard.co>
2021-07-25 21:01:46 +0800

    add bootstrp 4 modal in 一致性會議、說明會資料

--- js/all.js
+++ js/all.js
@@ -10,4 +10,14 @@ $(function() {
   // 投件清單日期選擇
   $('#startDate').datepicker();
   $('#endDate').datepicker();
+
+  // 一致性會議 modal
+  $('.meeting-modal').click(function() {
+    $('#meeting-modal').modal('show')
+  })
+
+  // 一致性會議 modal
+  $('.seminar-modal').click(function() {
+    $('#seminar-modal').modal('show')
+  })
 });
